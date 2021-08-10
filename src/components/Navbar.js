dd1adb6ec95b397db0f0b40a8a9e6e1fa472cc42
JhanaGUSTAVE <jhana.gustave@gmail.com>
2021-08-10 15:37:08 -0400

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+// import App from "./App";
+// export default App;
+
+import React, { Component } from 'react';
+// import Identicon from 'identicon.js';
+
+class Navbar extends Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a
+          className="navbar-brand col-sm-3 col-md-2 mr-0"
+          href="!#"
+        >
+          File Dapp
+        </a>
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+            <small className="text-secondary">
+              <small id="account">{this.props.account}</small>
+            </small>
+            { this.props.account
+              ? <img alt=""
+                className='ml-2'
+                width='30'
+                height='30'
+                src={`data:image/png;base64,${new(this.props.account, 30).toString()}`}
+              />
+              : <span></span>
+            }
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
